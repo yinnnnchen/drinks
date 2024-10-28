d9540a18be83a5af2309f4ffb1165e668ba2ceb5
owner <owner@C301-04>
2024-10-28 11:27:56 +0800

    加入專案檔案。

---
+++ drinks/MainWindow.xaml.cs
@@ -0,0 +1,214 @@
+﻿using Microsoft.Win32;
+using System.IO;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace drinks
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        Dictionary<string, int> drinks = new Dictionary<string, int>();
+
+        Dictionary<string, int> orders = new Dictionary<string, int>();
+        string takeout = "";
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            // 讀取飲料品項檔案
+            AddNewDrink(drinks);
+
+            // 顯示飲料品項
+            DisplayDrinkMenu(drinks);
+        }
+
+        private void AddNewDrink(Dictionary<string, int> drinks)
+        {
+            OpenFileDialog openFileDialog = new OpenFileDialog();
+            openFileDialog.Title = "選擇飲料品項檔案";
+            openFileDialog.Filter = "CSV文件|*.csv|文字檔案|*.txt|所有文件|*.*";
+
+            if (openFileDialog.ShowDialog() == true)
+            {
+                string fileName = openFileDialog.FileName;
+                string[] lines = File.ReadAllLines(fileName);
+
+                foreach (var line in lines)
+                {
+                    string[] tokens = line.Split(',');
+                    string drinkName = tokens[0];
+                    int price = Convert.ToInt32(tokens[1]);
+                    drinks.Add(drinkName, price);
+                }
+            }
+        }
+
+        private void DisplayDrinkMenu(Dictionary<string, int> drinks)
+        {
+            //stackpanel_DrinkMenu.Children.Clear();
+            stackpanel_DrinkMenu.Height = 42 * drinks.Count;
+            foreach (var drink in drinks)
+            {
+                var sp = new StackPanel
+                {
+                    Orientation = Orientation.Horizontal,
+                    Margin = new Thickness(3),
+                    Background = Brushes.LightBlue,
+                    Height = 35,
+                };
+
+                var cb = new CheckBox
+                {
+                    Content = drink.Key,
+                    FontFamily = new FontFamily("微軟正黑體"),
+                    FontSize = 16,
+                    FontWeight = FontWeights.Bold,
+                    Foreground = Brushes.Blue,
+                    Width = 150,
+                    Margin = new Thickness(5),
+                    VerticalContentAlignment = VerticalAlignment.Center,
+                };
+
+                var lb_price = new Label
+                {
+                    Content = $"{drink.Value}元",
+                    FontFamily = new FontFamily("微軟正黑體"),
+                    FontSize = 16,
+                    FontWeight = FontWeights.Bold,
+                    Foreground = Brushes.Green,
+                    Width = 60,
+                    VerticalContentAlignment = VerticalAlignment.Center,
+                };
+
+                var sl = new Slider
+                {
+                    Width = 150,
+                    Minimum = 0,
+                    Maximum = 10,
+                    Value = 0,
+                    Margin = new Thickness(5),
+                    VerticalAlignment = VerticalAlignment.Center,
+                    IsSnapToTickEnabled = true,
+                };
+
+                var lb_amount = new Label
+                {
+                    Content = "0",
+                    FontFamily = new FontFamily("微軟正黑體"),
+                    FontSize = 16,
+                    FontWeight = FontWeights.Bold,
+                    Foreground = Brushes.Red,
+                    VerticalContentAlignment = VerticalAlignment.Center,
+                    Width = 50,
+                };
+
+                Binding myBinding = new Binding("Value");
+                myBinding.Source = sl;
+                lb_amount.SetBinding(ContentProperty, myBinding);
+
+                sp.Children.Add(cb);
+                sp.Children.Add(lb_price);
+                sp.Children.Add(sl);
+                sp.Children.Add(lb_amount);
+
+                stackpanel_DrinkMenu.Children.Add(sp);
+            }
+
+        }
+
+        private void RadioButton_Checked(object sender, RoutedEventArgs e)
+        {
+            var rb = sender as RadioButton;
+            if (rb.IsChecked == true)
+            {
+                //MessageBox.Show(rb.Content.ToString());
+                takeout = rb.Content.ToString();
+            }
+        }
+
+        private void OrderButton_Click(object sender, RoutedEventArgs e)
+        {
+            // 確認訂購內容
+            orders.Clear();
+            for (int i = 0; i < stackpanel_DrinkMenu.Children.Count; i++)
+            {
+                var sp = stackpanel_DrinkMenu.Children[i] as StackPanel;
+                var cb = sp.Children[0] as CheckBox;
+                var drinkName = cb.Content.ToString();
+                var sl = sp.Children[2] as Slider;
+                var amount = (int)sl.Value;
+
+                if (cb.IsChecked == true && amount > 0) orders.Add(drinkName, amount);
+            }
+
+            // 顯示訂購內容
+            string msg = "";
+            string discount_msg = "";
+            int total = 0;
+
+            DateTime dateTime = DateTime.Now;
+            msg += $"訂購時間：{dateTime.ToString("yyyy/MM/dd HH:mm:ss")}，此次訂購為{takeout}，訂購內容如下：\n";
+
+            int num = 1;
+            foreach (var order in orders)
+            {
+                int subtotal = drinks[order.Key] * order.Value;
+                msg += $"{num}. {order.Key} x {order.Value}杯，小計{subtotal}元\n";
+                total += subtotal;
+                num++;
+            }
+            msg += $"總金額為{total}元";
+
+            int sellPrice = total;
+            if (total >= 500)
+            {
+                sellPrice = (int)(total * 0.8);
+                discount_msg = $"恭喜您獲得滿500元打8折優惠";
+            }
+            else if (total >= 300)
+            {
+                sellPrice = (int)(total * 0.9);
+                discount_msg = $"恭喜您獲得滿300元打9折優惠";
+            }
+            else
+            {
+                discount_msg = $"未達到任何折扣條件";
+            }
+            msg += $"\n{discount_msg}，原價為{total}元，售價為 {sellPrice}元。";
+
+            ResultTextBlock.Text = msg;
+
+            SaveFileDialog saveFileDialog = new SaveFileDialog();
+            saveFileDialog.Title = "儲存訂購內容";
+            saveFileDialog.Filter = "文字檔案|*.txt|所有文件|*.*";
+
+            if (saveFileDialog.ShowDialog() == true)
+            {
+                string fileName = saveFileDialog.FileName;
+                try
+                {
+                    using (StreamWriter sw = new StreamWriter(fileName))
+                    {
+                        sw.Write(msg);
+                    }
+                    MessageBox.Show("訂單已成功儲存。");
+                }
+                catch (IOException ex)
+                {
+                    MessageBox.Show($"儲存檔案時發生錯誤: {ex.Message}");
+                }
+            }
+        }
+    }
+}
